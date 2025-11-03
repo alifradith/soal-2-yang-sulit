4e9fa3e504de529fca1af8b438400398762ff4aa
Radith <radithganteng949@gmail.com>
2025-11-04 06:08:57 +0700

    Add files via upload

---
+++ sulit2.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+public class sulit2 {
+  
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        // Meminta input dimensi matriks
+        System.out.print("Masukkan jumlah baris matriks: ");
+        int baris = scanner.nextInt();
+        System.out.print("Masukkan jumlah kolom matriks: ");
+        int kolom = scanner.nextInt();
+        
+        // Deklarasi matriks
+        int[][] matriks1 = new int[baris][kolom];
+        int[][] matriks2 = new int[baris][kolom];
+        int[][] hasil = new int[baris][kolom];
+        
+        // Input elemen matriks pertama
+        System.out.println("Masukkan elemen matriks pertama:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print("Elemen [" + i + "][" + j + "]: ");
+                matriks1[i][j] = scanner.nextInt();
+            }
+        }
+        
+        // Input elemen matriks kedua
+        System.out.println("Masukkan elemen matriks kedua:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print("Elemen [" + i + "][" + j + "]: ");
+                matriks2[i][j] = scanner.nextInt();
+            }
+        }
+        
+        // Penjumlahan matriks
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                hasil[i][j] = matriks1[i][j] + matriks2[i][j];
+            }
+        }
+        
+        // Menampilkan hasil penjumlahan
+        System.out.println("Hasil penjumlahan matriks:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print(hasil[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
+}
+
+
